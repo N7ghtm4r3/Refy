f718f8f038aaec74322fe0c78eb1c278b093b49c
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2025-05-04 17:17:56 +0200

    Moved the translation from Mantis (not the web page provider)

--- core/src/commonMain/kotlin/com/tecknobit/refycore/helpers/RefyInputsValidator.kt
+++ core/src/commonMain/kotlin/com/tecknobit/refycore/helpers/RefyInputsValidator.kt
@@ -38,11 +38,6 @@ object RefyInputsValidator : InputsValidator() {
     private val colorRegex: Regex = Regex(HEX_COLOR_PATTERN)
 
     /**
-     * `WRONG_TAG_NAME` message to use when the tag name of the user is not valid
-     */
-    const val WRONG_TAG_NAME_MESSAGE: String = "wrong_tag_name_key"
-
-    /**
      * Method to validate a tag name
      *
      * @param tagName Tag name to check the validity
